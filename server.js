5948337c27d46a28e9b871aa75568aa186920c01
Nazar Kobernyk <104902964+inkogi-to@users.noreply.github.com>
2023-05-26 17:40:56 +0100

    test_mongoDB

--- server.js
+++ server.js
@@ -1,5 +1,16 @@
+const mongoose = require("mongoose")
+
+const DB_HOST = "mongodb+srv://kogitonk:sU6LNGjohQsSqraE@cluster0.rv8clre.mongodb.net/?retryWrites=true&w=majority"
+
+mongoose.set('strictQuery', true)
+
 const app = require('./app')
 
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000")
-})
+mongoose.connect(DB_HOST)
+    .then(() => {
+        app.listen(3000)
+    })
+    .catch(error => {
+        console.log(error.message);
+        process.exit(1)
+    })
